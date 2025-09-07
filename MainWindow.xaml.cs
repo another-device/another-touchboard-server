231955d5d1341fe84f5e8ea39f8d0d7b30eb0fe2
hua-zhi-wan <1847196775@qq.com>
2025-09-08 01:06:38 +0800

    lighter service discovery

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,31 +21,6 @@ namespace AnotherTouchboard
         private ObservableCollection<string> _connectedClientsList = new ObservableCollection<string>();
         private string _logText = "";
 
-        // 按键映射表，将字符串映射到VirtualKeyCode
-        private Dictionary<string, ushort> _keyCodeMap = new Dictionary<string, ushort>
-        {
-            {"A", 0x41}, {"B", 0x42}, {"C", 0x43}, {"D", 0x44}, {"E", 0x45}, {"F", 0x46},
-            {"G", 0x47}, {"H", 0x48}, {"I", 0x49}, {"J", 0x4A}, {"K", 0x4B}, {"L", 0x4C},
-            {"M", 0x4D}, {"N", 0x4E}, {"O", 0x4F}, {"P", 0x50}, {"Q", 0x51}, {"R", 0x52},
-            {"S", 0x53}, {"T", 0x54}, {"U", 0x55}, {"V", 0x56}, {"W", 0x57}, {"X", 0x58},
-            {"Y", 0x59}, {"Z", 0x5A},
-            {"0", 0x30}, {"1", 0x31}, {"2", 0x32}, {"3", 0x33}, {"4", 0x34},
-            {"5", 0x35}, {"6", 0x36}, {"7", 0x37}, {"8", 0x38}, {"9", 0x39},
-            {"F1", 0x70}, {"F2", 0x71}, {"F3", 0x72}, {"F4", 0x73}, {"F5", 0x74},
-            {"F6", 0x75}, {"F7", 0x76}, {"F8", 0x77}, {"F9", 0x78}, {"F10", 0x79},
-            {"F11", 0x7A}, {"F12", 0x7B},
-            {"ENTER", 0x0D}, {"BACKSPACE", 0x08}, {"TAB", 0x09}, {"ESCAPE", 0x1B},
-            {"SPACE", 0x20}, {"SHIFT", 0x10}, {"CONTROL", 0x11}, {"ALT", 0x12},
-            {"CAPSLOCK", 0x14}, {"NUMLOCK", 0x90}, {"SCROLLLOCK", 0x91},
-            {"UP", 0x26}, {"DOWN", 0x28}, {"LEFT", 0x25}, {"RIGHT", 0x27},
-            {"HOME", 0x24}, {"END", 0x23}, {"PAGEUP", 0x21}, {"PAGEDOWN", 0x22},
-            {"INSERT", 0x2D}, {"DELETE", 0x2E},
-            {"PLUS", 0xBB}, {"MINUS", 0xBD}, {"MULTIPLY", 0x6A}, {"DIVIDE", 0x6F},
-            {"COMMA", 0xBC}, {"PERIOD", 0xBE}, {"SLASH", 0xBF}, {"SEMICOLON", 0xBA},
-            {"EQUALS", 0xBB}, {"LEFTBRACKET", 0xDB}, {"RIGHTBRACKET", 0xDD},
-            {"BACKQUOTE", 0xC0}, {"APOSTROPHE", 0xDE}, {"BACKSLASH", 0xDC}
-        };
-
         public ObservableCollection<string> PressedKeys => _pressedKeys;
         public ObservableCollection<string> ConnectedClients => _connectedClientsList;
 
@@ -237,11 +212,19 @@ namespace AnotherTouchboard
 
                     Dispatcher.Invoke(() => AddLog($"开始在局域网广播 (IP: {localIp}, 端口: 8889)"));
 
-                    // 每5秒广播一次
+                    // 每2秒广播一次
                     while (!_cancellationTokenSource.Token.IsCancellationRequested)
                     {
-                        await _udpClient.SendAsync(data, data.Length, broadcastEndPoint);
-                        await Task.Delay(5000, _cancellationTokenSource.Token);
+                        if (_connectedClients.Count > 0)
+                        {
+                            // 有客户端连接时不广播
+                            await Task.Delay(10000, _cancellationTokenSource.Token);
+                        }
+                        else
+                        {
+                            await _udpClient.SendAsync(data, data.Length, broadcastEndPoint);
+                            await Task.Delay(2000, _cancellationTokenSource.Token);
+                        }
                     }
                 }
                 catch (OperationCanceledException)
